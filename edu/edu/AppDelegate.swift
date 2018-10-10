f9b2598121ca1167321778e6448f0e08a4d72cf1
Giannina Pachas <gianninapachas@gmail.com>
2018-10-10 16:51:32 -0500

    initial xcode project

---
+++ edu/edu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  edu
+//
+//  Created by Giannina Pachas on 10/10/18.
+//  Copyright © 2018 CSE3311-Team 5-(ZG,GP,CH,TM). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
