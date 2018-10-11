b004e8a405e577246ff22b81605c9b9c77cce0c1
Giannina Pachas <gianninapachas@gmail.com>
2018-10-11 12:53:58 -0500

    The right project to download

---
+++ edu/edu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  edU
+//
+//  Created by Giannina Pachas on 10/11/18.
+//  Copyright © 2018 CSE3311-Team 5-(ZG,GP,CH,TM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ edu/eduTests/eduTests.swift
@@ -0,0 +1,36 @@
+//
+//  edUTests.swift
+//  edUTests
+//
+//  Created by Giannina Pachas on 10/11/18.
+//  Copyright © 2018 CSE3311-Team 5-(ZG,GP,CH,TM). All rights reserved.
+//
+
+import XCTest
+@testable import edU
+
+class edUTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
